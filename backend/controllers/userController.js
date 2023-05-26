1c8934f0fb7fa96bfa5d11bad563c36365649dab
Tommy Miguel <tommymiguel@Tommys-MacBook-Pro.local>
2023-05-26 13:17:39 -0700

    configured the routing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,10 +4,41 @@ import asyncHandler from 'express-async-handler'
 // route POST /api/user/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(401)
-    throw new Error('Something went wrong')
+    res.status(200).json({ message: 'Auth User' })
+});
+
+// @desc Register a new user
+// route POST /api/user
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Register User' })
+});
+
+// @desc Logout User
+// route POST /api/user/logout
+// @access Public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Logout User' })
+});
+
+// @desc Get User Profile
+// route GET /api/user/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'User Profile' })
+});
+
+// @desc Update User Profile
+// route PUT /api/user/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Update User Profile' })
 });
 
 export {
     authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
 }
