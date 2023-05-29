ae0eb4a15f7331ecfed586b9f6954bdd53a87a5d
Tommy Miguel <tommymiguel@Tommys-MacBook-Pro.local>
2023-05-29 11:32:05 -0700

    jwt, cookies, errorHandler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,68 @@
 import asyncHandler from 'express-async-handler'
+import User from '../models/userModels.js';
+import generateToken from '../utils/generateTokens.js'
 
 // @desc Auth user/ set Token
 // route POST /api/user/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Auth User' })
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id)
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid email or password ')
+    }
 });
 
 // @desc Register a new user
 // route POST /api/user
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Register User' })
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already Exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        generateToken(res, user._id)
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid User data')
+    }
 });
 
 // @desc Logout User
 // route POST /api/user/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
     res.status(200).json({ message: 'Logout User' })
 });
 
